69141fac375568a1bb750fd820287274dcd9494b
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-08-22 15:13:59 +0530

    create object for Scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author KAPIL KUMAR
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String username = "";
+        scanner.close();
+
     }
 }
