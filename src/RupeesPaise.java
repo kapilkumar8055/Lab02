bd7898596d2bcf0abe926db7f8109bbf3ac51739
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-08-29 14:49:28 +0530

    take input from user

--- src/RupeesPaise.java
+++ src/RupeesPaise.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RupeesPaise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double rupees = new Scanner(System.in);
+        Scanner rupees = new Scanner(System.in);
 
     }
 
