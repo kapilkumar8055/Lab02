1e1e6a6da07c8f1dd19a123cd7927fe9f744afbf
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-08-29 14:58:38 +0530

    decleare the variable

--- src/Fahrenheit.java
+++ src/Fahrenheit.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Fahrenheit {
     public static void main(String[] args) {
-        Scanner temp = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
+        double F = input.nextDouble();
+        double celcius = (F - 32 / 1.8);
     }
 }
