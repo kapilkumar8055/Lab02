d2b9dd88894f4821499e6494a80eb246441eb382
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-01 21:53:30 +0530

    final commit

---
+++ src/Debugging/Program1.java
@@ -0,0 +1,9 @@
+package Debugging;
+
+public class Program1 {
+    public static void display() {
+        int x = 123456;
+        float f = 100.12f;
+        System.out.println("The float value = " + f);
+    }
+}
